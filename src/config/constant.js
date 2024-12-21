f1baac2444c62aa26307066ca2f1c44bb6590441
Ibrahim faisal <ibrahimbusiness24525@gmail.com>
2024-12-21 11:28:56 +0500

    url added

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://market-product-backend.vercel.app/';
-export const BASE_URL = 'http://localhost:8080/';
+export const BASE_URL = 'https://okiee-backend-production.up.railway.app/';
 export const BASE_TITLE = ' | Okiee';
 
 export const CONFIG = {
