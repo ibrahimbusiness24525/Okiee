621f02c4f9ccb73eea72acfea8e10745a71f7cd9
Ibrahim faisal <ibrahimbusiness24525@gmail.com>
2025-02-14 23:15:49 +0500

    add base url

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'https://market-product-backend.vercel.app/';
-export const BASE_URL = 'https://market-product-backend.vercel.app/';
-// export const BASE_URL = 'http://localhost:8000/';
+// export const BASE_URL = 'https://market-product-backend.vercel.app/';
+export const BASE_URL = 'http://localhost:8000/'; 
 export const BASE_TITLE = ' | Okiiee';
 
 export const CONFIG = {
