f4448e94ee4ac179b563b41e9d2e8bb7241d6290
Ibrahim faisal <ibrahimbusiness24525@gmail.com>
2025-04-09 11:25:44 +0500

    update

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = 'https://market-product-backend.vercel.app/';
 // export const BASE_URL = 'http://market-product-backend.vercel.app/';
-// export const BASE_URL = 'https://api.okiiee.com/';
-export const BASE_URL = 'http://localhost:8000/';
+export const BASE_URL = 'https://api.okiiee.com/';
+// export const BASE_URL = 'http://localhost:8000/';
 // export const BASE_URL = 'http://ibokiiee-env.eba-hbnhxpe8.us-east-1.elasticbeanstalk.com/';
 
     export const BASE_TITLE = ' | Okiiee';
