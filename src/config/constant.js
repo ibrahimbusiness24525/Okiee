81d9f60534025fee1f4d7dfcc034968ed53dda5d
arshmanahmad <arshmanmuhammad153@gmail.com>
2025-12-06 15:38:56 +0500

    feat: update

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://api.okiiee.com/';
-export const BASE_URL = 'http://localhost:8000/';
+export const BASE_URL = 'https://api.okiiee.com/';
+// export const BASE_URL = 'http://localhost:8000/';
 // export const BASE_URL = 'http://ibokiiee-env.eba-hbnhxpe8.us-east-1.elasticbeanstalk.com/';
 
 export const BASE_TITLE = ' | Okiiee';
