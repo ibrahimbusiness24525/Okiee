70c03b901eaaaa7ddc5860dfd0e865c5fed063c7
arshmanahmad <arshmanmuhammad153@gmail.com>
2026-01-24 14:34:45 +0500

    update

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://okiee-backend-production-a1b3.up.railway.app/';
-export const BASE_URL = 'http://localhost:8000/';
+export const BASE_URL = 'https://okiee-backend-production-a1b3.up.railway.app/';
+// export const BASE_URL = 'http://localhost:8000/';
 // export const BASE_URL = 'http://ibokiiee-env.eba-hbnhxpe8.us-east-1.elasticbeanstalk.com/';
 
 export const BASE_TITLE = ' | Okiiee';
